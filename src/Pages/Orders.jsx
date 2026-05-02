e882e71d2e50fd8b511988fcfcf2329cbc3797c6
Anita2061 <anitagyawali101@gmail.com>
2026-05-02 16:28:19 +0545

    fix all merge conflicts and Orders host bug

--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -12,11 +12,7 @@ const Orders = () => {
         const fetchOrders = async () => {
             if (!user) return;
             try {
-<<<<<<< HEAD
-                const res = await api.get('/accounts/orders/');
-=======
-                const res = await api.get(`${host}/accounts/orders/`);
->>>>>>> 0ade13d930a10823aa15e8300847af908b190491
+                const res = await api.get("/accounts/orders/");
                 setOrders(res.data);
             } catch (error) {
                 console.error("Failed to fetch orders:", error);
